e02dfa3fa8d2e58bed684c574454ab090dabc185
Romero Lijkwan <romerolijkwan@gmail.com>
2018-07-24 03:26:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,21 @@
-// declare variables for inputField, addTaskButton, clearTaskButton and UL
-var input                   = document.getElementById("inputField");
-var addTaskButton           = document.getElementById("addTask");
-var clearTaskButton         = document.getElementById("clearTask");
-var ulList                  = document.getElementById("listItems");
+// checking for false below prevents the change event from also firing
+addTaskButton.addEventListener('click', () => false ? addTask() : 'do not fire change event');
 
-// listen for changes in inputField and invoke addTask();
-input.addEventListener("change", function(){
-    addTask();
-})
+inputField.addEventListener('change', () => addTask());
 
-// listen for clicks on addTaskButton and invoke addTask();
-addTaskButton.addEventListener("click", function(){
-    addTask();
-})
+clearTaskButton.addEventListener('click', () => clearTask());
 
-// listen for clicks on clearTaskButton and invoke clearTask();
-clearTaskButton.addEventListener("click", function(){
-    clearTask();
-})
-
-// addTask function with code to be executed upon invocation
-function addTask(){
-    // add inputField value to new variable
-    var newTodo             = input.value;
-    // create new LI after inputField value captured in var newTodo
-    var newListItem         = document.createElement("li");
-    // add newly created LI to innerHTML
-    newListItem.innerHTML   += newTodo;
-    // show newly created LI on front-end to user
-    ulList.appendChild(newListItem);
-    // inputField value is cleared and ready for new input
-    input.value             = "";
+function addTask() {
+  var newListItem = document.createElement('li');
+  newListItem.innerHTML = inputField.value;
+  listItems.appendChild(newListItem);
+  inputField.value = ''; // reset field value to blank for next entry
 }
 
-// clearTask function with code to be executed upon invocation
-function clearTask(){
-    // remove all childNodes after cleartaskButton has been clicked. Got this code from https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_node_removechild_while
-    while (ulList.hasChildNodes()) {   
-        ulList.removeChild(ulList.firstChild);
-    }
+  /* remove all childNodes after cleartaskButton has been clicked. 
+Got this code from https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_node_removechild_while */
+function clearTask() {
+  while (listItems.hasChildNodes()) {
+    listItems.removeChild(listItems.firstChild);
+  }
 }
