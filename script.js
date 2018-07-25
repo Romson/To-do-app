27ab8db0c436c377c1b9bc7d5c762ba0ace682b5
romson <romerolijkwan@gmail.com>
2018-07-25 18:25:00 +0200

    script edited

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-// SO2RandellDawson tyvm m8
->>>>>>> 6fab40bf2debeaa3c5a3ef38efe0125fa4fef80a
 // checking for false below prevents the change event from also firing
 addTaskButton.addEventListener('click', () => false ? addTask() : 'do not fire change event');
 
@@ -9,8 +5,8 @@ inputField.addEventListener('change', () => addTask());
 
 clearTaskButton.addEventListener('click', () => clearTask());
 
-<<<<<<< HEAD
 taskDone.addEventListener('click', () => taskDone());
+
 
 // var taskDone = document.document.querySelector("input[type='checkbox']");
 
@@ -28,19 +24,11 @@ function addTask() {
     checkbox.id             = "taskDone";
     newListItem.appendChild(checkbox);
     inputField.value = ''; // reset field value to blank for next entry
-=======
-function addTask() {
-  var newListItem = document.createElement('li');
-  newListItem.innerHTML = inputField.value;
-  listItems.appendChild(newListItem);
-  inputField.value = ''; // reset field value to blank for next entry
->>>>>>> 6fab40bf2debeaa3c5a3ef38efe0125fa4fef80a
 }
 
-  /* remove all childNodes after cleartaskButton has been clicked. 
-Got this code from https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_node_removechild_while */
+//* remove all childNodes after cleartaskButton has been clicked. 
+// Got this code from https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_node_removechild_while */
 function clearTask() {
-<<<<<<< HEAD
     while (listItems.hasChildNodes()) {
         listItems.removeChild(listItems.firstChild);
     }
@@ -50,9 +38,3 @@ function clearTask() {
 function taskDone() {
     checkbox.newListItem.style.textDecoration = "line-through"; // childNode.parentNode ?
 }
-=======
-  while (listItems.hasChildNodes()) {
-    listItems.removeChild(listItems.firstChild);
-  }
-}
->>>>>>> 6fab40bf2debeaa3c5a3ef38efe0125fa4fef80a
